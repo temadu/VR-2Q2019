90e520b157ddee9702a48cdcadecc57d040bcfc5
Impervius <nicodonof@hotmail.com>
2019-11-23 17:24:15 -0300

    General scene fixes

---
+++ FireExtinguisher/Assets/Scripts/FireExpandant.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireExpandant : MonoBehaviour{
+    private float maybeFire;
+
+    // Start is called before the first frame update
+    void Start(){
+
+        maybeFire = 100; // 2 Seconds
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate(){
+        if (maybeFire > 0){
+            maybeFire--;
+        }else{
+            maybeFire = 100;
+            Collider[] posibleOnFireObjects = Physics.OverlapSphere( transform.position , 2F);
+            foreach (var item in posibleOnFireObjects){
+                if( Random.Range(0,3) > 0  && 
+                    item.gameObject.GetInstanceID() != transform.parent.gameObject.GetInstanceID() && 
+                    item.gameObject.tag == "Flamable" &&
+                    item.transform.childCount == 0
+                    ){
+                        Debug.Log(item.name + item.GetInstanceID());
+                        Debug.Log(transform.parent.gameObject.name + transform.parent.gameObject.GetInstanceID());
+                    GameObject newFire = Instantiate(this.gameObject, item.gameObject.transform);
+                    newFire.transform.position = item.transform.position;
+                }
+            }
+        }    
+    }
+}
---
+++ FireExtinguisher/Assets/Scripts/FireLordBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireLordBehaviour : MonoBehaviour{
+
+    public GameObject fire;
+    public List<GameObject> createdFire;
+    private float maybeFire;
+    // Start is called before the first frame update
+    void Start(){
+        GameObject[] flamables = GameObject.FindGameObjectsWithTag("Flamable");
+        int rand = Random.Range(0, flamables.Length);
+        createdFire = new List<GameObject>();
+
+        createdFire.Add(Instantiate(fire, flamables[rand].transform));
+        float height = flamables[rand].GetComponent<MeshCollider>().bounds.size.y;
+        createdFire[0].transform.position = flamables[rand].transform.position + new Vector3(0,height + 0.2F,0);
+
+    }
+
+    void FixedUpdate(){
+        
+    }
+    
+}
