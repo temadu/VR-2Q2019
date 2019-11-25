d2bbd17f94617a6eb79519ebb77c4e64dbee7c54
Teofilo Manuel Duffau <tduffau@itba.edu.ar>
2019-11-25 14:35:05 -0300

    Add smoke

---
+++ FireExtinguisher/Assets/Scripts/SmokeReleaser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmokeReleaser : MonoBehaviour
+{
+    ParticleSystem particleSystem;
+
+    void Start() {
+         particleSystem = GetComponent<ParticleSystem>();
+         particleSystem.Stop();
+    }
+
+    void Update() {
+      if(Input.GetMouseButton(0)){
+        particleSystem.Play();
+      } else {
+        particleSystem.Stop();
+      }
+    }
+}
