ae78a5ca1e40fe4b6f4d9e9803165db9a334f963
rcarreir <rcarreir@itba.edu.ar>
2019-12-13 11:12:29 -0300

    Create Firekiller.cs

---
+++ FireExtinguisher/Assets/Firekiller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firekiller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0) || OVRInput.Get(OVRInput.RawButton.LIndexTrigger))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(gameObject.transform.position, gameObject.transform.forward, out hit))
+            {
+                Debug.Log(hit.transform.name);
+                if (hit.collider.tag == "Fire")
+                {
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+        }
+    }
+}
