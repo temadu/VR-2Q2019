65003dbaecb7d88a8da8cbea2bcf27db41841226
Teofilo Manuel Duffau <tduffau@itba.edu.ar>
2019-12-15 13:35:03 -0300

    Add marc fixes

--- FireExtinguisher/Assets/Firekiller.cs
+++ FireExtinguisher/Assets/Firekiller.cs
@@ -13,7 +13,7 @@ public class Firekiller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0) || OVRInput.Get(OVRInput.RawButton.LIndexTrigger))
+        if (Input.GetMouseButton(0) || OVRInput.Get(OVRInput.RawButton.RIndexTrigger))
         {
             RaycastHit hit;
             if (Physics.Raycast(gameObject.transform.position, gameObject.transform.forward, out hit))
